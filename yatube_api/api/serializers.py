08de3f05a6cf82bea121220347a55a417dbe9f31
Yury Zharikov <zharikovyuri@gmail.com>
2022-09-26 11:32:40 +0300

    for review2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
+    author = serializers.SlugRelatedField(read_only=True,
+                                          slug_field='username')
 
     class Meta:
         model = Post
